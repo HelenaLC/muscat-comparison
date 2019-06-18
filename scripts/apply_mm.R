39a65d31701923aed3ff910197eafc1b724d13b7
HelenaLC <helena@crowells.eu>
2019-06-18 18:04:08 +0200

    silence MM

--- scripts/apply_mm.R
+++ scripts/apply_mm.R
@@ -12,7 +12,7 @@ apply_mm <- function(sce, pars, ds_only = TRUE) {
             sce$dr <- colMeans(counts(sce) > 0)
         pars[sapply(pars, `==`, "")] <- NULL
         res <- tryCatch(
-            do.call(mmDS, c(list(sce, n_threads = 1, verbose = TRUE), pars)),
+            do.call(mmDS, c(list(sce, n_threads = 1, verbose = FALSE), pars)),
             error = function(e) e)
         if (!inherits(res, "error"))
             res <- dplyr::bind_rows(res)
