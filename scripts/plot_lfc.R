24a75800cde2e64d3ddd6f7af05bdfdeda772403
HelenaLC <helena@crowells.eu>
2019-07-11 10:53:31 +0200

    prettify; use ggrastr to reduce filesize

--- scripts/plot_lfc.R
+++ scripts/plot_lfc.R
@@ -8,40 +8,43 @@ suppressPackageStartupMessages({
     library(purrr)
 })
 
+#fns <- list.files("~/documents/kang", "d[a-z][0-9]+;", full.names = TRUE)
 df <- lapply(snakemake@input$res, readRDS) %>% 
-    map("tbl") %>% bind_rows %>% 
-    filter(!is.na(est_lfc)) %>% 
-    mutate_at("mid", factor, levels = names(.meth_cols)) %>% 
-    mutate_at("mid", droplevels) %>% 
-    rename(method = mid)
+    map("tbl") %>% 
+    map(mutate_if, is.factor, as.character) %>% 
+    bind_rows %>% 
+    dplyr::filter(!is.na(sim_lfc) & !is.na(est_lfc)) %>% 
+    rename(method = mid) %>% 
+    mutate_at("method", function(u) droplevels(
+        factor(u, levels = names(.meth_cols)))) %>% 
+    mutate_at("sid", factor, 
+        levels = paste0(c("ds", "dp", "dm", "db"), "10"),
+        labels = c("DS", "DP", "DM", "DB"))
 
-ps <- group_by(df, sid) %>% {set_names(group_split(.), group_keys(.)[[1]])} %>% lapply(function(u) 
-    ggplot(u, aes(x = sim_lfc, y = est_lfc, col = as.logical(is_de))) +
-        facet_wrap(~ method, ncol = 3) +
-        geom_point_rast(size = 0.1, alpha = 0.1) +
-        scale_color_manual(values = c("FALSE" = "royalblue", "TRUE" = "tomato")) +
-        guides(color = guide_legend("DD", override.aes = list(size = 3, alpha = 1))) +
-        scale_x_continuous(limits = c(-7, 7), breaks = seq(-6, 6, 3), expand = c(0, 0)) +
-        scale_y_continuous(limits = c(-7, 7), breaks = seq(-6, 6, 3), expand = c(0, 0)) +
-        labs(x = "simulated logFC", y = "estimated logFC") +
-        .prettify("bw") + theme(
-            strip.text = element_text(size = 5),
-            panel.spacing = unit(0.1, "cm"),
-            legend.position = "bottom"))
+# dowsample to 2k cluster-gene combinations
+# per method, simulation & relicate
+set.seed(29)
+sub <- ungroup(sample_n(group_by(df, method, sid, i), 2e3))
 
-lgd <- get_legend(ps[[1]])
-ps <- lapply(ps, "+", theme(legend.position = "none"))
-names(ps) <- toupper(gsub("[0-9]", "", names(ps)))
-ps <- ps[c("DS", "DP", "DM", "DB")]
+# cut 0.1% quantiles
+rng <- quantile(c(sub$sim_lfc, sub$est_lfc), c(1e-3, 1-1e-3))
+sub <- mutate_at(sub, c("sim_lfc", "est_lfc"), function(u) {
+    u[u < rng[1]] <- rng[1]; u[u > rng[2]] <- rng[2]; return(u) })
+rng <- ceiling(max(abs(c(sub$sim_lfc, sub$est_lfc))))
 
-p <- plot_grid(
-    plotlist = c(ps, list(lgd)), 
-    ncol = 1,
-    rel_heights = c(rep(1, 4), 0.2),
-    labels = names(ps),
-    label_size = 10,
-    label_fontface = "bold")
+p <- ggplot(sub, aes(x = sim_lfc, y = est_lfc, col = as.logical(is_de))) +
+    facet_grid(vars(sid), vars(method)) +
+    geom_point_rast(size = 1.2, alpha = 0.4, raster.dpi = 100) +
+    scale_color_manual(values = c("FALSE" = "royalblue", "TRUE" = "tomato")) +
+    guides(color = guide_legend("differential", override.aes = list(size = 3, alpha = 1))) +
+    scale_x_continuous(limits = c(-rng, rng), expand = c(0, 1)) +
+    scale_y_continuous(limits = c(-rng, rng), expand = c(0, 1)) +
+    labs(x = "simulated logFC", y = "estimated logFC") +
+    .prettify("bw") + theme(
+        strip.text = element_text(size = 4),
+        panel.spacing = unit(1, "mm"),
+        legend.position = "bottom")
 
 ggsave(snakemake@output$fig, p,
-    width = 15, height = 24, units = "cm",
+    width = 15, height = 11, units = "cm",
     dpi = 300, useDingbats = FALSE)
