923c593be86b8e0114526efe38a821003a828ee3
HelenaLC <helena@crowells.eu>
2019-07-11 10:58:04 +0200

    prettify

--- scripts/plot_lfc.R
+++ scripts/plot_lfc.R
@@ -30,15 +30,14 @@ sub <- ungroup(sample_n(group_by(df, method, sid, i), 2e3))
 rng <- quantile(c(sub$sim_lfc, sub$est_lfc), c(1e-3, 1-1e-3))
 sub <- mutate_at(sub, c("sim_lfc", "est_lfc"), function(u) {
     u[u < rng[1]] <- rng[1]; u[u > rng[2]] <- rng[2]; return(u) })
-rng <- ceiling(max(abs(c(sub$sim_lfc, sub$est_lfc))))
 
 p <- ggplot(sub, aes(x = sim_lfc, y = est_lfc, col = as.logical(is_de))) +
     facet_grid(vars(sid), vars(method)) +
     geom_point_rast(size = 1.2, alpha = 0.4, raster.dpi = 100) +
     scale_color_manual(values = c("FALSE" = "royalblue", "TRUE" = "tomato")) +
     guides(color = guide_legend("differential", override.aes = list(size = 3, alpha = 1))) +
-    scale_x_continuous(limits = c(-rng, rng), expand = c(0, 1)) +
-    scale_y_continuous(limits = c(-rng, rng), expand = c(0, 1)) +
+    scale_x_continuous(limits = c(-6,6), breaks = seq(-6,6,3), expand = c(0, 1)) +
+    scale_y_continuous(limits = c(-6,6), breaks = seq(-6,6,3), expand = c(0, 1)) +
     labs(x = "simulated logFC", y = "estimated logFC") +
     .prettify("bw") + theme(
         strip.text = element_text(size = 4),
