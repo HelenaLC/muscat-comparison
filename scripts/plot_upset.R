bf10a7c2d106aa484f4d3019232f32273dd6e9d0
HelenaLC <helena@crowells.eu>
2019-07-18 12:31:04 +0200

    scale y sqrt

--- scripts/plot_upset.R
+++ scripts/plot_upset.R
@@ -1,6 +1,6 @@
 source(snakemake@config$utils)
 
-suppressPackageStartupMessages({
+suppressMessages({
     library(cowplot)
     library(dplyr)
     library(ggplot2)
@@ -8,11 +8,9 @@ suppressPackageStartupMessages({
     library(purrr)
 })
 
-#fns <- list.files("results/kang", "d[a-z]10;", full.names = TRUE)
-res <- lapply(snakemake@input$res, readRDS) %>%
-    map("tbl") %>% bind_rows %>% 
-    mutate(hit = paste(gene, cluster_id, sid, i, sep = ";")) %>% 
-    mutate_at("category", plyr::revalue, c("de" = "ds"))
+fns <- list.files("results/kang", "d[a-z]10;", full.names = TRUE)
+res <- .read_res(fns) %>% 
+    mutate(hit = paste(gene, cluster_id, sid, i, sep = ";"))
 
 n_dd <- res %>% 
     dplyr::filter(mid == .$mid[1]) %>% 
@@ -20,16 +18,14 @@ n_dd <- res %>%
     summarize(n_dd = sum(is_de)) %>% 
     acast(sid ~ i, value.var = "n_dd")
 
-top <- res %>% group_by(sid, i) %>% do({
+top <- group_by(res, sid, i) %>% do({
     n <- n_dd[.$sid[1], .$i[1]]
-    arrange(., p_val) %>%
+    filter(., p_adj.loc < 0.05) %>% 
+        arrange(p_val) %>% 
         group_by(mid, add = TRUE) %>%
         slice(seq_len(n)) %>% 
         summarize(hit = list(hit))
 }) %>% group_by(mid) %>% summarize(hit = list(reduce(hit, c)))
-
-.cat_cols <- c("royalblue", "cornflowerblue", "red3", "tomato", "orange", "gold")
-names(.cat_cols) <- levels(res$category)
 
 df <- UpSetR::fromList(set_names(top$hit, top$mid)) %>% mutate(
     code = apply(.[top$mid], 1, paste, collapse = ""),
@@ -38,30 +34,29 @@ df <- UpSetR::fromList(set_names(top$hit, top$mid)) %>% mutate(
         m <- match(.$hit, res$hit)
         mutate(., sid = res$sid[m], i = res$i[m], cat = res$category[m])
     } %>% add_count(code) %>% group_by(code) %>% 
-    mutate(p_true = 100 * sum(cat %in% c("ds", "dp", "dm", "db")) / n) %>% 
+    mutate(p_true = mean(!cat %in% c("ee", "ep"))) %>% 
     ungroup
 
 m <- match(unique(df$code), df$code)
-keep <- pull(top_n(df[m, ], 50, n), "code")
+keep <- pull(top_n(df[m, ], 40, n), "code")
 df <- filter(df, code %in% keep)
 m <- match(unique(df$code), df$code)
 o <- order(df$degree[m], -df$n[m])
 
-#/ (sum(n_dd) * nrow(top))
-max <- ceiling(max(df$n)/500)*500
+max <- ceiling(max(df$n)/1e3)*1e3
 p1 <- ggplot(df, aes(x = code)) +
-    coord_cartesian(clip = 'off') +
-    geom_bar(aes(y = ..count.., fill = cat)) + 
-    geom_point(shape = 17, size = 1, col = "lightgrey", 
-        aes(y = p_true * max / 100)) +
+    stat_count(aes(fill = cat)) + 
+    geom_point(aes(y = p_true * max),
+        shape = 17, size = 1, col = "grey75") +
     scale_x_discrete(limits = df$code[m][o]) +
     scale_y_continuous(limits = c(0, max), expand = c(0,0),
         sec.axis = sec_axis(~./max(.), breaks = seq(0, 1, 0.2))) +
+    coord_trans(y = "sqrt", clip = "off") +
     scale_fill_manual(NULL, values = .cat_cols, 
         labels = function(u) toupper(u)) +
     .prettify("classic") + theme(
         legend.margin = margin(0,0,0,0, "mm"),
-        plot.margin = unit(c(2,2,2,0), "mm"),
+        plot.margin = unit(c(2,2,2,2), "mm"),
         legend.key.size = unit(4, "mm"),
         panel.grid.major.x = element_blank(),
         axis.line = element_blank(),
@@ -69,15 +64,17 @@ p1 <- ggplot(df, aes(x = code)) +
         axis.text.x = element_blank(),
         axis.title = element_blank(),
         aspect.ratio = NULL)
-
-lgd <- get_legend(p1)
+p1
+lgd <- get_legend(p1 + theme(legend.margin = margin(0,0,0,0, "mm")))
 p1 <- p1 + theme(legend.position = "none")
 
-dfm <- melt(df, id.var = setdiff(names(df), top$mid), variable.name = "method")
+dfm <- melt(df, variable.name = "method",
+    id.var = setdiff(names(df), top$mid)) %>% 
+    group_by(code, method) %>% slice(1)
 p2 <- ggplot(dfm, aes(x = code, y = method, color = factor(value))) +
     scale_x_discrete(limits = df$code[m][o]) +
     scale_y_discrete(limits = rev(top$mid)) +
-    scale_color_manual(values = c("0" = "grey92", "1" = "black")) +
+    scale_color_manual(values = c("0" = "grey90", "1" = "black")) +
     geom_point(shape = 16, size = 1) +
     geom_path(size = 0.2, data = filter(dfm, value != 0), aes(group = code)) +
     annotate("rect", alpha = 0.08, xmin = 0.5, xmax = Inf, 
@@ -92,18 +89,18 @@ p2 <- ggplot(dfm, aes(x = code, y = method, color = factor(value))) +
         axis.title = element_blank(),
         aspect.ratio = NULL) 
 
-anno <- read.csv(snakemake@config$mids)
-anno$type <- factor(anno$type, levels = unique(anno$type[match(top$mid, anno$id)]))
-.typ_cols <- setNames(RColorBrewer::brewer.pal(nlevels(anno$type), "Set2"), levels(anno$type))
+anno <- read.csv("meta/meth_ids.csv")
+anno$type <- factor(anno$type, levels = c("pb", "mm", "ad", "scdd", "mast"))
+.typ_cols <- setNames(RColorBrewer::brewer.pal(9, "Set3")[c(1,3,4,5,6)], levels(anno$type))
 
 p3 <- ggplot(anno, aes(x = 0, y = id, fill = type)) +
-    scale_fill_manual("type", values = .typ_cols,
-        labels = c(ad = "AD", mast = "MAST", pb = "PB", scdd = "scDD")) +
-    geom_tile(col = "white") + 
+    scale_fill_manual("method", values = .typ_cols,
+        labels = c(pb = "PB", mm = "MM", ad = "AD", mast = "MAST", scdd = "scDD")) +
+    geom_tile(col = "white") + coord_fixed(0.6) +
     scale_y_discrete(limits = rev(top$mid)) +
     .prettify("classic") + theme(aspect.ratio = NULL,
-        legend.margin = margin(0,0,0,0, "mm"),
-        plot.margin = unit(c(0,2,0,0), "mm"),
+        legend.margin = margin(0,-4,0,0, "mm"),
+        plot.margin = unit(c(0,6,0,0), "mm"),
         panel.grid = element_blank(),
         axis.line = element_blank(),
         axis.ticks = element_blank(),
@@ -115,9 +112,10 @@ p <- plot_grid(
         align = "v", axis = "lr", 
         rel_heights = c(3, 2)),
     plot_grid(lgd, p3, ncol = 1, 
-        align = "v", axis = "l", 
+        align = "hv", axis = "tl", 
         rel_heights = c(3, 2)),
-    nrow = 1, rel_widths = c(6, 1))
+    nrow = 1, rel_widths = c(7, 1))
 
-ggsave(snakemake@output$fig, p, dpi = 300,
-    width = 15, height = 10, units = "cm")
+ggsave("figures/kang/upset.pdf", p, dpi = 300,
+    width = 15, height = 8, units = "cm")
+
