18dc3fb0458b163f6c83a7585cadbdb274f348ad
HelenaLC <helena@crowells.eu>
2019-07-25 09:34:43 +0200

    plot runtimes vs. nb. genes/cells

--- scripts/plot_runtimes.R
+++ scripts/plot_runtimes.R
@@ -6,33 +6,59 @@ suppressMessages({
     library(purrr)
 })
 
-fns <- list.files("/Users/helena/Dropbox/portmac/results/kang", "ds10_nc;", full.names = TRUE)
-res <- lapply(fns, readRDS)
-tbl <- map(res, "tbl")
-rmv <- vapply(tbl, inherits, what = "error", logical(1))
-nms <- basename(fns)[!rmv]
-
-rts <- map(res[!rmv], "rt") %>% 
+#fns <- list.files("results/kang", "ds10_n[g|c];", full.names = TRUE)
+pat <- "%s;%s;%s;%s;g%s;c%s;k%s;s%s"
+tbl <- .read_res(snakemake@input$res) %>% 
+    dplyr::mutate(id = sprintf(pat, sid, i, mid, j, g, c, k, s))
+rts <- .read_res(fns, slot = "rt") %>% 
     vapply(sum, numeric(1)) %>% 
-    set_names(nms)
+    set_names(gsub(".rds", "", basename(fns)))
 
-df <- map(tbl[!rmv], mutate_if, is.factor, as.character) %>% 
-    set_names(nms) %>% bind_rows(.id = "id")
+m <- match(tbl$id, names(rts))
+df <- data.frame(tbl, rt = rts[m],
+    stringsAsFactors = FALSE) %>% 
+    mutate_at(c("c", "g"), function(u)
+        as.numeric(as.character(u))) %>% 
+    mutate_at("c", function(u) u * 2 * 2 * 3) %>% 
+    mutate_at("g", function(u) u * 2)
 
-df <- df[match(nms, df$id), ] %>% 
-    mutate(t = rts) %>% 
-    mutate(id = paste(mid, c, sep = "--")) %>% 
-    mutate_at("c", as.numeric)
+ps <- lapply(c("c", "g"), function(x) {
+   u <- dplyr::filter(df, sid == paste0("ds10_n", x)) %>% 
+       dplyr::mutate(id = paste(mid, get(x), sep = "--")) %>% 
+       group_by(id, i) %>% dplyr::slice(1)
+    
+    ss <- strsplit(unique(u$id), "--")
+    m <- sapply(ss, .subset, 1)
+    n <- sapply(ss, .subset, 2)
+    o1 <- order(as.numeric(n))
+    o2 <- match(m, levels(u$mid))
+    lvls <- unique(u$id)[order(o2)[o1]]
+    u$id <- factor(u$id, levels = lvls)
+    
+    ggplot(u, aes_string(x = x, y = "rt", group = "id", col = "mid", fill = "mid")) +
+        geom_boxplot(alpha = 0.8, size = 0.4, outlier.size = 0.4) + 
+        scale_y_log10() + 
+        scale_x_log10(breaks = unique(u[[x]])) +
+        scale_color_manual(NULL, values = .meth_cols) +
+        scale_fill_manual(NULL, values = .meth_cols) +
+        guides(col = guide_legend(ncol = 3)) +
+        labs(x = sprintf("nb. of %s", c(c = "cells", g = "genes")[x]), y = "runtime (s)") +
+        .prettify("bw") + theme(legend.position = "bottom", aspect.ratio = 2/3)
+})
 
-ggplot(df, aes(x = c, y = t, group = id, col = mid, fill = mid)) +
-    geom_smooth(method = "lm", aes(group = mid), size = 0.2, alpha = 0.05) + 
-    geom_boxplot(alpha = 0.8) + scale_y_log10() +
-    scale_x_log10(breaks = unique(df$c)) +
-    scale_color_manual(NULL, values = .meth_cols) +
-    scale_fill_manual(NULL, values = .meth_cols, guide = FALSE) +
-    labs(x = "#(cells) per cluster-sample", y = "runtime (s)") +
-    .prettify("bw") + theme(aspect.ratio = 2/3)
+lgd <- get_legend(ps[[1]])
+ps <- lapply(ps, "+", theme(legend.position = "none"))
+
+p <- plot_grid(
+    plot_grid(nrow = 1,
+        plotlist = ps,  
+        labels = "auto", 
+        label_size = 10, 
+        label_fontface = "bold"),
+    lgd, ncol = 1, 
+    rel_heights = c(4, 1))
 
 ggsave(snakemake@output$fig, p,
-    units = "cm", width = 7, height = 4,
+    units = "cm", width = 15, height = 6.4,
     dpi = 300, useDingbats = FALSE)    
+
