b2745bd7b78edaa5f9c2f4736401946b8031b00c
HelenaLC <helena@crowells.eu>
2019-08-06 11:45:27 +0200

    assure cluster IDs are factors

--- scripts/lps_run_meth.R
+++ scripts/lps_run_meth.R
@@ -3,15 +3,8 @@ suppressMessages(library(SingleCellExperiment))
 
 # load data & method parameters
 sce <- readRDS(snakemake@input$sce)
+sce$cluster_id <- factor(sce$cluster_id)
 meth_pars <- as.list(jsonlite::fromJSON(snakemake@input$meth_pars))
-
-# compute new assay slot if required
-if (meth_pars$id == "edgeR.sum(scalecpm)")
-    assays(sce)$cpm <- scater::calculateCPM(sce)
-
-if (isTRUE(grep("vstresiduals", meth_pars$id) == 1))
-    assays(sce)$vstresiduals <- suppressWarnings(
-        sctransform::vst(counts(sce), show_progress = FALSE)$y)
 
 # get method wrapper 'apply_xx()'
 source(fun <- snakemake@input$fun)
