e532635d2e2f7a71e82dbb10e34723a0d391766d
HelenaLC <helena@crowells.eu>
2019-08-08 14:44:05 +0200

    simplify code

--- scripts/lps_plot_upset.R
+++ scripts/lps_plot_upset.R
@@ -15,16 +15,17 @@ res <- lapply(fns, readRDS) %>% map("tbl") %>%
     set_names(gsub("(.rds)", "", basename(fns)))
 
 rmv <- vapply(res, inherits, what = "error", logical(1))
-df <- bind_rows(res[!rmv], .id = "method_id") %>% 
+df <- bind_rows(res[!rmv]) %>% 
     mutate_at("method_id", factor, levels = names(.meth_cols)) %>% 
     mutate_at("method_id", droplevels)
 
 gs <- filter(df, p_adj.loc < 0.05) %>% 
-    split(.$method_id) %>% map(pull, "gene")
+    split(.$method_id) %>% 
+    map(pull, "gene")
 
 p <- upset(fromList(gs), 
     sets = levels(df$method_id),
-    nintersects = 50,
+    nintersects = 80,
     mb.ratio = c(0.6, 0.4))
 p    
     
