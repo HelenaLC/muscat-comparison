2a656776e9acb3d7b56d7fa027bc366394984787
HelenaLC <helena@crowells.eu>
2019-08-08 14:42:10 +0200

    assure complete result dfs are returned; limit to 1 thread

---
+++ scripts/lps_plot_upset.R
@@ -0,0 +1,30 @@
+source(snakemake@config$utils)
+
+# load packages
+suppressMessages({
+    library(dplyr)
+    library(purrr)
+    library(UpSetR)
+})
+
+# load results
+snakemake@input$res
+
+fns <- list.files("results/lps", full.names = TRUE)
+res <- lapply(fns, readRDS) %>% map("tbl") %>% 
+    set_names(gsub("(.rds)", "", basename(fns)))
+
+rmv <- vapply(res, inherits, what = "error", logical(1))
+df <- bind_rows(res[!rmv], .id = "method_id") %>% 
+    mutate_at("method_id", factor, levels = names(.meth_cols)) %>% 
+    mutate_at("method_id", droplevels)
+
+gs <- filter(df, p_adj.loc < 0.05) %>% 
+    split(.$method_id) %>% map(pull, "gene")
+
+p <- upset(fromList(gs), 
+    sets = levels(df$method_id),
+    nintersects = 50,
+    mb.ratio = c(0.6, 0.4))
+p    
+    
--- scripts/lps_run_meth.R
+++ scripts/lps_run_meth.R
@@ -3,16 +3,21 @@ suppressMessages(library(SingleCellExperiment))
 
 # load data & method parameters
 sce <- readRDS(snakemake@input$sce)
-sce$cluster_id <- factor(sce$cluster_id)
 meth_pars <- as.list(jsonlite::fromJSON(snakemake@input$meth_pars))
 
 # get method wrapper 'apply_xx()'
 source(fun <- snakemake@input$fun)
 fun <- gsub("(.R)", "", basename(fun))
 
-if (isTRUE(grep("MM", meth_pars$id) == 1))
-    meth_pars$n_threads <- 10
-
 # run method & write results to .rds
 res <- get(fun)(sce, meth_pars, ds_only = FALSE)
+if (!inherits(res$tbl, "error")) {
+    foo <- data.frame(
+        gene = rep(rownames(sce), nk),
+        cluster_id = rep(kids, each = nrow(sce)),
+        method_id = meth_pars$id,
+        stringsAsFactors = FALSE)
+    res$tbl <- left_join(foo, res$tbl, 
+        by = c("gene", "cluster_id"))
+}
 saveRDS(res, snakemake@output$res)
