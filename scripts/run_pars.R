af983e76ab8481090e3b0c863e6a76c453fc950d
HelenaLC <helena@crowells.eu>
2019-08-12 17:18:01 +0200

    dd category ds > de

--- scripts/run_pars.R
+++ scripts/run_pars.R
@@ -5,13 +5,13 @@ def_pars <- list(nr = 1, nk = "x", ns = "x", ng = "x", nc = "x", seed = 1)
 
 run_pars <- list(
     kang = list(
-        ds10_ng = list(nr = 1, ng = c(500, 1e3, 2e3, 4e3)),
-        ds10_nc = list(nr = 1, nc = c(50, 100, 200, 400)),
-        ds10_ns = list(nr = 1, ns = c(2, 3, 4, 5))
+        de10_ng = list(nr = 1, ng = c(500, 1e3, 2e3, 4e3)),
+        de10_nc = list(nr = 1, nc = c(50, 100, 200, 400)),
+        de10_ns = list(nr = 1, ns = c(2, 3, 4, 5))
     ),
     magl = list(
-        ds10_nc = list(nr = 1, nc = c(50, 100, 200, 400)),
-        ds10_ng = NULL, ds10_ns = NULL
+        de10_nc = list(nr = 1, nc = c(50, 100, 200, 400)),
+        de10_ng = NULL, de10_ns = NULL
     )
 )
 
