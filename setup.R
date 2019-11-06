e565edea26065e2a2b6d771c9b8a2a1b6008d53f
HelenaLC <helena@crowells.eu>
2019-11-06 12:34:34 +0100

    fix type

--- setup.R
+++ setup.R
@@ -1,6 +1,6 @@
 config <- yaml::read_yaml("config.yaml")
 x <- c("sim_pars", "run_pars", "meth_pars", 
-    "raw_data" "sim_data", "results", "figures")
+    "raw_data", "sim_data", "results", "figures")
 for (dir in unlist(config[x]))
     if (!dir.exists(dir) & !isTRUE(grep("\\.", dir) == 1))
         dir.create(dir, recursive = TRUE)
