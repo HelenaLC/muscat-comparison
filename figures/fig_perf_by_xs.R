e4d87c5d6571f9d76cf3db762a5aa829f52aeaa3
HelenaLC <helena@crowells.eu>
2020-02-11 15:04:25 +0100

    add fig for perf by sample/group size

--- figures/fig_perf_by_xs.R
+++ figures/fig_perf_by_xs.R
@@ -8,8 +8,10 @@ suppressMessages({
     library(reshape2)
 })
 
+for (x in c("s", "g")) {
+
 # load performance plots
-fns <- paste0(config$dids, "-perf_by_ss.rds")
+fns <- sprintf("%s-perf_by_%ss.rds", config$dids, x)
 ps <- lapply(file.path("plots", fns), readRDS)
 lgd1 <- get_legend(ps[[1]])
 ps <- lapply(ps, "+", theme(legend.position = "none",
@@ -20,27 +22,39 @@ ps <- lapply(ps, "+", theme(legend.position = "none",
 sids <- unique(unlist(map(map(ps, "data"), "sid")))
 fns <- paste0(config$sim_pars, sids, ".json")
 sim_pars <- lapply(fns, yaml::read_yaml)
-n <- sapply(c("nk", "ns", "nc"), function(u) unlist(map(sim_pars, u)))
-n <- with(as.data.frame(n), nc / (2 * nk * ns))
-ss <- map(map(sim_pars, "probs"), 2) 
-ss <- sapply(seq_along(ss), function(i) n[i] * ss[[i]]) 
+names(ns) <- ns <- c("nk", "ns", "nc")
+ns <- lapply(ns, function(u) unlist(map(sim_pars, u)))
+nc <- with(as.data.frame(n), nc / (2 * nk * ns))
+ss <- map(map(sim_pars, "probs"), ifelse(x == "s", 2, 3)) 
+ss <- sapply(seq_along(ss), function(i) nc[i] * ss[[i]])
 colnames(ss) <- gsub(".json", "", basename(fns))
 
-gg_df <- melt(ss) %>% 
-    set_colnames(c("sample_id", "sim_id", "n")) %>% 
-    replicate(n = 2, simplify = FALSE) %>% 
-    bind_rows(.id = "group_id") %>% 
-    mutate_at("group_id", function(u) factor(u, labels = c("A", "B"))) %>% 
+if (x == "s") {
+    gg_df <- melt(ss) %>% 
+        set_colnames(c("sample_id", "sim_id", "n")) %>% 
+        replicate(n = 2, simplify = FALSE) %>% 
+        bind_rows(.id = "group_id")
+} else {
+    gg_df <- melt(ss) %>% 
+        set_colnames(c("group_id", "sim_id", "n")) %>% 
+        replicate(n = ns$ns[1], simplify = FALSE) %>% 
+        bind_rows(.id = "sample_id")
+}
+gg_df <-  mutate_at(gg_df, "group_id", 
+    function(u) factor(u, labels = c("A", "B"))) %>% 
     dplyr::mutate(sample_id = factor(paste0(group_id, sample_id)))
+
+ns <- nlevels(gg_df$sample_id)/2
+cols <- lapply(c("Blues", "Reds"), function(pal)
+    brewer.pal(9, pal)[seq(7, 9-2*ns, -2)])
 
 p0 <- ggplot(gg_df, aes(x = sample_id, y = n, fill = sample_id)) +
     facet_wrap("sim_id", nrow = 1) +
     geom_bar(size = 1, width = 0.5, stat = "identity") +
-    scale_fill_manual(values = c(
-        brewer.pal(9, "Blues")[c(7,5,3)], 
-        brewer.pal(9, "Reds")[c(7,5,3)])) +
-    scale_y_continuous("expected\nnb. of cells", limits = c(0, 125)) +
-    guides(fill = guide_legend(ncol = 3, byrow = TRUE,
+    scale_fill_manual(values = unlist(cols)) +
+    scale_y_continuous("expected\nnb. of cells", expand = c(0, 0), 
+        limits = c(0, ceiling(max(gg_df$n)/50)*50)) +
+    guides(fill = guide_legend(ncol = ns, byrow = TRUE,
         override.aes = list(size = 0))) +
     .prettify("bw") + theme(
         aspect.ratio = NULL,
@@ -68,11 +82,9 @@ p <- plot_grid(p, l, ncol = 1,
     align = "v", axis = "r",
     rel_heights = c(8, 1))
 
-ggsave(file.path("figures", "perf_by_ss.pdf"), 
-    p, width = 15, height = 12.5, units = "cm",
+fn <- file.path("figures", sprintf("perf_by_%ss.pdf", x))
+ggsave(fn, p, 
+    width = 15, height = 12.5, units = "cm",
     dpi = 300, useDingbats = FALSE)
 
-
-
-
-
+}
