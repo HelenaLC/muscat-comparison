2855169f42c4dede13173a65cee136fca353cffa
HelenaLC <helena@crowells.eu>
2020-02-11 15:20:53 +0100

    fix inputt files

--- figures/fig_perf_combined.R
+++ figures/fig_perf_combined.R
@@ -1,17 +1,14 @@
-config <- yaml::read_yaml("config.yaml")
-source(config$utils)
-
 suppressMessages({
     library(cowplot)
     library(ggplot2)
 })
 
-id <- "kang"
+ref_id <- "kang"
 
-p1 <- readRDS(file.path("figures", id, "perf_by_cat.rds"))
-p2 <- readRDS(file.path("figures", id, "perf_by_nc.rds"))
+ps <- list(
+    readRDS(file.path("figures", paste0(ref_id, "-perf_by_cat.rds"))),
+    readRDS(file.path("plots", paste0(ref_id, "-perf_by_nc.rds"))))
 
-ps <- list(p1, p2)
 lgd <- get_legend(ps[[1]])
 ps <- lapply(ps, "+", theme(legend.position = "none"))
 
@@ -22,7 +19,8 @@ p <- plot_grid(ncol = 1,
     label_size = 10,
     label_fontface = "bold")
 
-ggsave(file.path("figures", id, "perf_combined.pdf"), p,
+fn <- file.path("figures", paste0(ref_id, "-perf_combined.pdf"))
+ggsave(fn, p,
     width = 15, height = 15, units = "cm", 
     dpi = 300, useDingbats = FALSE)   
 
