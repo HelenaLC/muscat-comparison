2fd8cbae745870ee63ecd0e37e7bd6e27bc2a6d0
HelenaLC <helena@crowells.eu>
2020-02-15 15:11:20 +0100

    exclude clusters w <200 cells as sim ref

--- scripts/prep_sim.R
+++ scripts/prep_sim.R
@@ -6,8 +6,10 @@ ref <- readRDS(args$input_sce)
 
 # prep. SCE for simulation w/ 'muscat::simData'
 sce <- prepSim(ref, verbose = FALSE,
-    min_count = 1, min_cells = 10,   # keep genes w/ count > 1 in >= 10 cells
-    min_genes = 100, min_size = 100) # keep cells w/ >= 100 detected genes
+    # keep genes w/ count > 1 in >= 10 cells
+    min_count = 1, min_cells = 10,   
+    # keep cells w/ >= 100 detected genes & cluster w/ > 100 cells
+    min_genes = 100, min_size = 200) 
 
 # write SCE to .rds
 saveRDS(sce, args$output_sce)
