b8cf3c324a385bce2a8c5425237874c8683befc4
HelenaLC <helena@crowells.eu>
2020-02-15 15:40:06 +0100

    WT>Vehicle

--- scripts/prep_magl.R
+++ scripts/prep_magl.R
@@ -11,6 +11,6 @@ suppressMessages({
 sce <- readRDS(args$input_sce) # load data
 reducedDims(sce) <- NULL # remove dimension reductions
 assays(sce) <- SimpleList(counts = counts(sce)) # remove slots other than counts
-sce <- sce[, sce$group_id == "WT"] # keep reference samples only
+sce <- sce[, sce$group_id == "Vehicle"] # keep reference samples only
 sce <- prepSCE(sce, "cluster_id", "sample_id", "group_id", TRUE) # prep. SCE for 'muscat'
 saveRDS(sce, args$output_sce) # write SCE to .rds
