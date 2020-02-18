8f45d5d8f19fd71e987c7be773609bd8dd5a7b97
HelenaLC <helena@crowells.eu>
2020-02-18 09:26:21 +0100

    min_size 200 > 100

--- scripts/prep_sim.R
+++ scripts/prep_sim.R
@@ -9,7 +9,7 @@ sce <- prepSim(ref, verbose = FALSE,
     # keep genes w/ count > 1 in >= 10 cells
     min_count = 1, min_cells = 10,   
     # keep cells w/ >= 100 detected genes & cluster w/ > 100 cells
-    min_genes = 100, min_size = 200) 
+    min_genes = 100, min_size = 100) 
 
 # write SCE to .rds
 saveRDS(sce, args$output_sce)
