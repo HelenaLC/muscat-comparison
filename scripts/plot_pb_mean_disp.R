2f4e9aadaf3e94b211115bd3f1cff4aaac9c49c3
HelenaLC <helena@crowells.eu>
2020-02-26 16:18:13 -0500

    minorprettification

--- scripts/plot_pb_mean_disp.R
+++ scripts/plot_pb_mean_disp.R
@@ -43,14 +43,23 @@ df <- dplyr::filter(res,
 
 p <- ggplot(df, aes(x = mean, col = id)) +
     facet_wrap(~ cluster_id, nrow = 1) + 
+    scale_x_continuous(
+        limits = c(0, 18), 
+        breaks = seq(0, 18, 6), 
+        expand = c(0.05, 0)) +
+    scale_y_continuous(
+        trans = "log10", 
+        limits = c(0.01, 0.3), 
+        expand = c(0.05, 0)) +
     geom_point_rast(aes(y = disp_tag), size = 0.2, alpha = 0.02) + 
     geom_point_rast(aes(y = disp_fit), size = 0.1, alpha = 0.1) +
     scale_color_manual(NULL, values = c("royalblue", "tomato")) +
     guides(color = guide_legend(override.aes = list(size = 2, alpha = 1))) +
-    labs(x = "mean logCPM", y = "dispersion") + scale_y_log10() + 
+    labs(x = "mean logCPM", y = "dispersion") +
     .prettify("bw", legend.position = "bottom")
 
 saveRDS(p, args$ggp)
 ggsave(args$fig, p,
     width = 15, height = 6, units = "cm", 
     dpi = 300, useDingbats = FALSE)
+
